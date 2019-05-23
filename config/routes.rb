2ebbd39d59290f099ab979380d1abe41dbdf6f33
Camille Regnault <c.regnault.pro@gmail.com>
2019-05-23 16:04:08 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :places
-  resources :bookings, only: (:new, :create, :show, :index, :delete)
+  resources :bookings, only: [:new, :create, :show, :index, :delete]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
