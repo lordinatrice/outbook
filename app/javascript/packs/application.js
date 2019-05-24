acb4d1eca31f82483e19f5090537fa194f78a7eb
antoine leprince <leprince.antoine7@gmail.com>
2019-05-24 15:44:52 +0200

    conflict to check

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
 import "bootstrap";
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
+
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+initUpdateNavbarOnScroll();
