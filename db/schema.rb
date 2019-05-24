5d53b9be680b84eed4e8455d20bdee2c841c9263
Eugenie Colonna d Istria <eugeniecolonnadistria@gmail.com>
2019-05-24 18:00:57 +0200

    to be deleted

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_05_24_143322) do
+ActiveRecord::Schema.define(version: 2019_05_24_150241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
