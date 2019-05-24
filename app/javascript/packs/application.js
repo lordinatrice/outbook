1c6ea1f0d18240706675a45c905e20833fdffcfb
antoine leprince <leprince.antoine7@gmail.com>
2019-05-24 14:53:17 +0200

    navbar transparent + banner

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
 import "bootstrap";
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+initUpdateNavbarOnScroll();
+
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
