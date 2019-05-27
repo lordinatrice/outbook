5a6337f7b2e9892170a5cedbe01bfaf2825d37ed
Sandra Vilder <sandra.vilder@gmail.com>
2019-05-27 11:56:58 +0200

    added map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,11 @@ initUpdateNavbarOnScroll();
 
 import { loadDynamicBannerText } from './components/banner';
 loadDynamicBannerText();
+
+// import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+// import { initMapbox } from '../plugins/init_mapbox';
+
+// initMapbox();
+
+// import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_150241) do
+ActiveRecord::Schema.define(version: 2019_05_27_082914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_05_24_150241) do
     t.string "name"
     t.string "photo2"
     t.string "photo3"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
