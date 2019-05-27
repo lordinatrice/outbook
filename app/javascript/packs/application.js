ac6ca32a6879ca7aa64c99ce72c5e88347c21bcb
Camille Regnault <c.regnault.pro@gmail.com>
2019-05-27 15:14:31 +0200

    debug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,16 +2,10 @@ import "bootstrap";
 import { initUpdateNavbarOnScroll } from './components/navbar';
 initUpdateNavbarOnScroll();
 
-<<<<<<< HEAD
-
 import { loadDynamicBannerText } from './components/banner';
 if (document.getElementById("banner-typed-text")) {
 loadDynamicBannerText();
 }
-=======
-// import { loadDynamicBannerText } from './components/banner';
-// loadDynamicBannerText();
->>>>>>> b70aa4bcc62486d92699f04000dd6c44b26ed185
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
