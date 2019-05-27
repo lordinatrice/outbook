b42eb03b4df43d305b281a0918d296ecd06ee012
Eugenie Colonna d Istria <eugeniecolonnadistria@gmail.com>
2019-05-27 15:05:57 +0200

    commit to pull

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,10 @@ import "bootstrap";
 import { initUpdateNavbarOnScroll } from './components/navbar';
 initUpdateNavbarOnScroll();
 
-import { loadDynamicBannerText } from './components/banner';
-loadDynamicBannerText();
-
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+// import { loadDynamicBannerText } from './components/banner';
+// loadDynamicBannerText();
 
 import { initMapbox } from '../plugins/init_mapbox';
 
