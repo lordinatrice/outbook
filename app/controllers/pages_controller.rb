3d80b4a4a275242ca14c4f1ca70f0f105227077b
Eugenie Colonna d Istria <eugeniecolonnadistria@gmail.com>
2019-05-28 12:25:40 +0200

    dashboard in one tabs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  set_tab :dashboard
 
   def home
   end
 
   def dashboard
     @places = current_user.places
+    @bookings = current_user.bookings
+    skip_policy_scope
   end
 end
