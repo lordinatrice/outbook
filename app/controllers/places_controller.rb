f4e20dfecf4b888d1aa589db6af40a0bc08a009c
antoine leprince <leprince.antoine7@gmail.com>
2019-05-28 12:50:36 +0200

    calendar show

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,6 +14,7 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find(params[:id])
+    @booking = Booking.new
     authorize @place
   end
 
---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,4 @@
+import flatpickr from "flatpickr"
+import "flatpickr/dist/themes/material_green.css" // Note this is important!
+
+flatpickr(".datepicker", {})
