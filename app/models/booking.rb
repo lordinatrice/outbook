92f39796b0975011fa2f821788b7b8d196957b74
Eugenie Colonna d Istria <eugeniecolonnadistria@gmail.com>
2019-05-29 14:07:50 +0200

    dashboard modified : added pedning bookings, missing js

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,7 @@
 class Booking < ApplicationRecord
+  STATUS = ["pending", "accepted", "refused"]
   belongs_to :user
   belongs_to :place
   validates :date, presence: true
+  validates :status, presence: true, inclusion: { in: STATUS }
 end
